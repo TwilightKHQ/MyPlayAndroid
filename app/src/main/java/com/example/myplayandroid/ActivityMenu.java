44a0ae49d3c1d362cdc7051e7e4d378ae38c86ba
zhiqiang.zhong <gengyingbo@sim.com>
2019-04-17 10:24:12 +0800

    基本完成检查更新(下载页面需要自定义Dialog、暂时未处理)

--- app/src/main/java/com/example/myplayandroid/ActivityMenu.java
+++ app/src/main/java/com/example/myplayandroid/ActivityMenu.java
@@ -1,6 +1,5 @@
 package com.example.myplayandroid;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
@@ -15,10 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,15 +27,11 @@ import com.google.gson.Gson;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.Response;
 
 
@@ -49,38 +41,13 @@ public class ActivityMenu extends AppCompatActivity {
 
     private DownloadTask downloadTask;
 
-
+    private ProgressDialog progressDialog;
 
     private AdapterSetting adapterSetting = new AdapterSetting(itemList);
 
     public String versionName;
 
     public int versionCode;
-
-    public static final int TYPE_SUCCESS = 0;
-    public static final int TYPE_FAILED = 1;
-    public static final int TYPE_PAUSED = 2;
-    public static final int TYPE_CANCELED = 3;
-
-    private boolean isCanceled = false;
-
-    private boolean isPaused = false;
-
-//    private Handler mhandler = new Handler(new Handler.Callback() {
-//        @Override
-//        public boolean handleMessage(Message msg) {
-//            //使用Handler制造一个200毫秒为周期的循环
-//            handler.sendEmptyMessageDelayed(1, 200);
-//            //计算下载进度
-//            int l = (int) ((float) info.getCompletedLen() / (float) info.getContentLen() * 100);
-//            //设置进度条进度
-//            bar.setProgress(l);
-//            if (l>=100) {//当进度>=100时，取消Handler循环
-//                handler.removeCallbacksAndMessages(null);
-//            }
-//            return true;
-//        }
-//    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,7 +72,6 @@ public class ActivityMenu extends AppCompatActivity {
         initItems();
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.menu_recycle_view);
-//        CardView cardView = (CardView) findViewById(R.id.card_view);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 1);
         recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.setAdapter(adapterSetting);
@@ -122,6 +88,7 @@ public class ActivityMenu extends AppCompatActivity {
                         AlertDialog.Builder dialog = new AlertDialog.Builder(ActivityMenu.this);
                         dialog.setTitle("版本更新");
                         dialog.setMessage("即将开始下载最新版本安装包");
+
                         dialog.setCancelable(true);
                         dialog.setPositiveButton("确认", new DialogInterface.OnClickListener() {
                             @Override
@@ -145,25 +112,23 @@ public class ActivityMenu extends AppCompatActivity {
 
         startDownload(downloadUrl);
 
-        ProgressDialog progressDialog = new ProgressDialog(ActivityMenu.this);
+        progressDialog = new ProgressDialog(ActivityMenu.this);
         progressDialog.setTitle("版本更新");
-        progressDialog.setMessage("Loading...");
-        progressDialog.setCancelable(true);
+        progressDialog.setMessage("正在下载最新安装包...");
+        progressDialog.setCancelable(false);
         progressDialog.setCanceledOnTouchOutside(false);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         progressDialog.setMax(100);
         progressDialog.setButton(DialogInterface.BUTTON_NEUTRAL, "取消", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                isCanceled= true;
                 cancelDownload(downloadUrl);
             }
         });
         progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "暂停", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                isPaused = true;
-                pauseDownload(downloadUrl);
+                pauseDownload();
             }
         });
         progressDialog.show();
@@ -173,7 +138,7 @@ public class ActivityMenu extends AppCompatActivity {
         //用getNotification()构建用于显示下载进度的通知， notify()方法用于触发通知
         @Override
         public void onProgress(int progress) {
-//            progressDialog.setProgress(progress);
+            progressDialog.setProgress(progress);
         }
 
         @Override
@@ -181,6 +146,7 @@ public class ActivityMenu extends AppCompatActivity {
             downloadTask = null;
             //下载成功时将前台服务通知关闭，
             Toast.makeText(ActivityMenu.this, "Download Success", Toast.LENGTH_SHORT).show();
+            progressDialog.cancel();
         }
 
         @Override
@@ -205,7 +171,6 @@ public class ActivityMenu extends AppCompatActivity {
 
     private void startDownload(String downloadUrl) {
         if (downloadTask == null) {
-            downloadUrl = downloadUrl;
             downloadTask = new DownloadTask(listener);
             downloadTask.execute(downloadUrl);
             //创建持续运行的通知
@@ -213,7 +178,7 @@ public class ActivityMenu extends AppCompatActivity {
         }
     }
 
-    public void pauseDownload(String downloadUrl) {
+    public void pauseDownload() {
         if (downloadTask != null) {
             downloadTask.pauseDownload();
         }
@@ -238,7 +203,7 @@ public class ActivityMenu extends AppCompatActivity {
 
     private void initItems() {
         itemList.clear();
-        Item item0 = new Item("当前版本号: " + versionName, R.mipmap.information);
+        Item item0 = new Item("当前版本:  " + versionName, R.mipmap.information);
         itemList.add(item0);
         Item item1 = new Item("检查更新", R.mipmap.update);
         itemList.add(item1);
@@ -297,8 +262,6 @@ public class ActivityMenu extends AppCompatActivity {
                 case 0x1:
                     Toast.makeText(ContextApplication.getContext(), "检测到更新", Toast.LENGTH_SHORT).show();
                     break;
-                case 0x2:
-
             }
         }
     };
